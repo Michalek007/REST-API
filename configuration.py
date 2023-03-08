b2d69e7cd860cab7aa15bc0a435dd21e661fa306
Michalek007 <nmichal02@op.pl>
2023-03-08 01:37:18 +0100

    Added: venvSetup.bat, run.bat, server.py, config.json, modified: configuration.py

--- configuration.py
+++ configuration.py
@@ -3,8 +3,12 @@ import os
 
 class Config:
     """App configuration."""
-    SERVER_NAME = '127.0.0.1:5000'
     SCHEDULER_API_ENABLED = True
+    LISTENER = {
+        'host': '127.0.0.1',
+        'port': 5000,
+    }
+    SERVER_NAME = f'{LISTENER["host"]}:{LISTENER["port"]}'
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'database\\data.db')
     JWT_SECRET_KEY = "hkBxrbZ9Td4QEwgRewV6gZSVH4q78vBia4GBYuqd09SsiMsIjH"
